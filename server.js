661335fb48c7ec21294e9609f4512cde59f41324
ShaunThayil <scthayil@gmail.com>
2017-02-07 07:42:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/Article-one',function(req,res){
-    res.sendFile("Article one will be displayed here");
+    res.sendFile(path.join(_dirname,'ui','Article-one.html'));
 });
 app.get('/Article-two',function(req,res){
     res.send("Article  two will be displayed here");
