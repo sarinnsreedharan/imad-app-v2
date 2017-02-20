2e996718f903bcb64134bf43b88422d5f9500c65
ShaunThayil <scthayil@gmail.com>
2017-02-20 21:28:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
-console.log('Loaded!');
+var counter=0;
+var button=document.getElementById("counter");
+
+button.onclick=function(){
+    counter=countet+1;
+    var span=document.getElementById("val");
+    span.innerHTML=counter.toString();
+    
+}
